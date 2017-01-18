dcdf32af092fedf40dc46aa6aab313aeb62ab5c7
niisar <nisar2048@gmail.com>
2017-01-19 00:21:38 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  stringArray: string[];
+  objectArray: Object[];
+  complexObjectArray: Object;
+
+  constructor() {
+    this.stringArray = ['India', 'Afgan', 'New York'];
+    this.objectArray = [
+      { name: 'Nisar', mark: 35, city: 'India' },
+      { name: 'John', mark: 12, city: 'Afgan' },
+      { name: 'Amy', mark: 22, city: 'New York' }
+    ];
+    this.complexObjectArray = [
+      {
+        city: 'India',
+        people: [
+          { name: 'Amy', mark: 12 },
+          { name: 'Lisa', mark: 22 }
+        ]
+      },
+      {
+        city: 'Afgan',
+        people: [
+          { name: 'John', mark: 35 },
+          { name: 'Mary', mark: 36 }
+        ]
+      }
+    ];
+  };
 }
